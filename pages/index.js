85d0a64192791e08e9e6a4898fedd812a34da7dd
cardfaux <cardfaux@tutanota.com>
2021-10-30 10:42:28 -0500

    testing some components

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
-import { Heading, Page } from "@shopify/polaris";
+import { Heading, Page, Card, Layout } from "@shopify/polaris";
+import { PlusMinor } from "@shopify/polaris-icons";
 
 const Index = () => (
-  <Page>
+  <Page
+    fullWidth
+    title="Orders"
+    primaryAction={{ content: "Create order", icon: PlusMinor }}
+    secondaryActions={[{ content: "Export" }]}
+    pagination={{
+      hasNext: true,
+    }}
+  >
     <Heading>Shopify app with Node and React 🎉</Heading>
+    <Layout>
+      <Layout.Section>
+        <Card title="Order details" sectioned>
+          <p>View a summary of your order.</p>
+        </Card>
+      </Layout.Section>
+      <Layout.Section secondary>
+        <Card title="Tags" sectioned>
+          <p>Add tags to your order.</p>
+        </Card>
+      </Layout.Section>
+    </Layout>
   </Page>
 );
 
