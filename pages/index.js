54b71212daa25ff19d63d56e67f9568945269ab6
cardfaux <cardfaux@tutanota.com>
2021-10-31 17:39:30 -0500

    set up some more pages with dummy content/frontend only

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,12 @@
-import { Heading, Page, Card, Layout } from "@shopify/polaris";
-import { TitleBar } from "@shopify/app-bridge-react";
-import { PlusMinor } from "@shopify/polaris-icons";
+import EmptyHome from "../components/EmptyHome";
+import FAQList from "../components/FAQList";
 
 const Index = (props) => {
-  console.log(props);
   return (
-    <Page
-      fullWidth
-      title="Orders"
-      primaryAction={{ content: "Create order", icon: PlusMinor }}
-      secondaryActions={[{ content: "Export" }]}
-      pagination={{
-        hasNext: true,
-      }}
-    >
-      <TitleBar title="Homepage" />
-      <Heading>Shopify app with Node and React 🎉</Heading>
-      <Layout>
-        <Layout.Section>
-          <Card title="Order details" sectioned>
-            <p>View a summary of your order.</p>
-          </Card>
-        </Layout.Section>
-        <Layout.Section secondary>
-          <Card title="Tags" sectioned>
-            <p>Add tags to your order.</p>
-          </Card>
-        </Layout.Section>
-      </Layout>
-    </Page>
+    <>
+      <EmptyHome />
+      <FAQList />
+    </>
   );
 };
 
